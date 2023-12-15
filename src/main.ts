0f93bfab6407e803e5fb7dfaff4103b3c8d6d244
Supanut1911 <ynotmela@gmail.com>
2023-12-15 15:21:55 +0700

    ✨ feat: add configService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configService } from './configs/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ origin: '*' });
+  await app.listen(configService.getPort(), () =>
+    console.log(`server listen on ${configService.getPort()}`),
+  );
 }
 bootstrap();
