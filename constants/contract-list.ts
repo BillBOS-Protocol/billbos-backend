cede3039bc8246f34659852be58476af55c37705
Supanut1911 <ynotmela@gmail.com>
2023-12-16 18:14:28 +0700

    ✨ feat: implement call contract to send contract and reset view at day 1 of monther 00:00

---
+++ constants/contract-list.ts
@@ -0,0 +1,6 @@
+export const contractAddress: Record<string, any>[] = [
+  {
+    contractName: 'greetContract',
+    contractAddress: '0xe95dFbCc7F2506c664589e995d4dE74cd2CE451F',
+  },
+];
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './configs/config.service';
 import { AdsModule } from './modules/ads/ads.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()), AdsModule],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    AdsModule,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
